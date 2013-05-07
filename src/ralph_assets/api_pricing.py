01013d60b957984959295f2237673c55bbaf4178
Wojciech Matyskiewicz <wojtek@matyskiewicz.com>
2013-05-07 13:25:28 +0200

    fix models name

--- src/ralph_assets/api_pricing.py
+++ src/ralph_assets/api_pricing.py
@@ -5,11 +5,11 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from ralph_assets.models_assets import models_assets
+from ralph_assets.models_assets import Asset
 
 def get_assets():
     """Yields dicts describing all assets"""
-    for asset in Assets.objects.all():
+    for asset in Asset.objects.all():
         yield {
             'name': asset.sn,
             'asset_id': asset.id,
