a3260d0f0669e2882d9eca5a399858758a287e04
Marcin Kliks <marcin.kliks@gmail.com>
2013-05-14 10:27:19 +0200

    Explicitly add discovery into the routers list

--- src/ralph_assets/routers.py
+++ src/ralph_assets/routers.py
@@ -7,3 +7,8 @@ class RalphAssetsRouter(BaseRouter):
     db_name = 'ralph_assets'
     app_name = 'ralph_assets'
 
+
+class RalphDiscoveryRouter(BaseRouter):
+    db_name = 'ralph'
+    app_name = 'discovery'
+
