96f464ef37395f3e71af0cddebcd48108c3f4af7
Wojciech Matyskiewicz <wojtek@matyskiewicz.com>
2013-05-15 14:46:26 +0200

    hell yeah!

--- src/ralph_assets/api_pricing.py
+++ src/ralph_assets/api_pricing.py
@@ -10,10 +10,9 @@ from ralph_assets.models_assets import Asset
 def get_assets():
     """Yields dicts describing all assets"""
     for asset in Asset.admin_objects_dc.all():
-    	ralph_device = asset.device_info.ralph_device
         yield {
             'asset_id': asset.id,
-            'ralph_id': ralph_device.id if ralph_device else None,
+            'ralph_id': asset.device_info.ralph_device_id,
             'slots': asset.slots,
             'price': asset.price,
             'is_deprecated': asset.is_deprecated()
