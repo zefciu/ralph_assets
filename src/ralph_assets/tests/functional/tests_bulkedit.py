206c62402a9e4b9dc3ca1249047aa836b7461424
Kula <kula1922@gmail.com>
2014-02-03 17:28:14 +0100

    create common config generator

--- src/ralph_assets/tests/functional/tests_bulkedit.py
+++ src/ralph_assets/tests/functional/tests_bulkedit.py
@@ -12,6 +12,7 @@ from ralph_assets.tests.util import (
     create_asset,
     create_model,
     create_category,
+    get_bulk_edit_post_data,
 )
 from ralph.ui.tests.global_utils import login_as_su
 
@@ -44,42 +45,32 @@ class TestBulkEdit(TestCase):
         content = self.client.get(url)
         self.assertEqual(content.status_code, 200)
 
-        # Prepare data for send via form
-        post_data = {
-            'form-TOTAL_FORMS': u'2',
-            'form-INITIAL_FORMS': u'2',
-            'form-MAX_NUM_FORMS': u'',
-            'form-0-id': 1,
-            'form-0-type': AssetType.data_center.id,
-            'form-0-model': self.model.id,
-            'form-0-invoice_no': 'Invoice No1',
-            'form-0-order_no': 'Order No1',
-            'form-0-invoice_date': '2012-02-02',
-            'form-0-sn': '3333-3333-3333-3333',
-            'form-0-barcode': 'bc-3333-3333-3333-3333',
-            'form-0-support_period': 24,
-            'form-0-support_type': 'standard1',
-            'form-0-support_void_reporting': 'on',
-            'form-0-provider': 'Provider1',
-            'form-0-price': 10,
-            'form-0-status': AssetStatus.in_progress.id,
-            'form-0-source': AssetSource.shipment.id,
-            'form-1-id': 2,
-            'form-1-type': AssetType.data_center.id,
-            'form-1-model': self.model1.id,
-            'form-1-invoice_no': 'Invoice No2',
-            'form-1-order_no': 'Order No2',
-            'form-1-invoice_date': '2011-02-03',
-            'form-1-sn': '4444-4444-4444-4444',
-            'form-1-barcode': 'bc-4444-4444-4444-4444',
-            'form-1-support_period': 48,
-            'form-1-support_type': 'standard2',
-            'form-1-support_void_reporting': 'off',
-            'form-1-provider': 'Provider2',
-            'form-1-price': 12,
-            'form-1-status': AssetStatus.waiting_for_release.id,
-            'form-1-source': AssetSource.shipment.id,
-        }
+        post_data = get_bulk_edit_post_data(
+            {
+                'model': self.model.id,
+                'invoice_no': 'Invoice No1',
+                'order_no': 'Order No1',
+                'invoice_date': '2012-02-02',
+                'support_period': 24,
+                'support_type': 'standard1',
+                'provider': 'Provider1',
+                'status': AssetStatus.in_progress.id,
+                'sn': '3333-3333-3333-3333',
+                'barcode': 'bc-3333-3333-3333-3333',
+            },
+            {
+                'model': self.model1.id,
+                'invoice_no': 'Invoice No2',
+                'order_no': 'Order No2',
+                'invoice_date': '2011-02-03',
+                'support_period': 48,
+                'support_type': 'standard2',
+                'provider': 'Provider2',
+                'status': AssetStatus.waiting_for_release.id,
+                'sn': '4444-4444-4444-4444',
+                'barcode': 'bc-4444-4444-4444-4444',
+            },
+        )
 
         response = self.client.post(url, post_data, follow=True)
 
