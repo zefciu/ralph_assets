5fc2e91f8b4b251dfb8808071f76e6cd4b0809e1
Kamil Warguła <kwargula@gmail.com>
2014-03-25 14:59:04 +0000

    Transition history on asset view

---
+++ src/ralph_assets/templatetags/assets_tags.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from django import template
+
+
+register = template.Library()
+
+
+@register.inclusion_tag('assets/templatetags/transition_history.html',)
+def transition_history(asset):
+    transitions_history = None
+    if hasattr(asset, 'transitionshistory_set'):
+        transitions_history = asset.transitionshistory_set.all()
+    return {'transitions_history': transitions_history}
