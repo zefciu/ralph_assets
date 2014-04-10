7766c9754ba159810d10d92271c0164e37a4b81a
Kamil Warguła <kwargula@gmail.com>
2014-04-10 13:12:38 +0000

    Better affected user detection

--- src/ralph_assets/views_transition.py
+++ src/ralph_assets/views_transition.py
@@ -194,10 +194,9 @@ class TransitionView(_AssetSearch):
             return self.form.cleaned_data.get('warehouse')
 
     def get_affected_user(self, *args, **kwargs):
-        if 'return-asset' in self.transition_object.name:
+        affected_user = self.form.cleaned_data.get('user')
+        if not affected_user:
             affected_user = self.assets[0].user
-        else:
-            affected_user = self.form.cleaned_data.get('user')
         return affected_user
 
     def get_report_file_link(self, *args, **kwargs):
@@ -240,7 +239,7 @@ class TransitionView(_AssetSearch):
             self.assign_warehouse = (
                 'assign_warehouse' in self.transition_object.actions_names()
             )
-        if self.transition_type == 'return-asset':
+        if self.transition_type == 'return-asset' or not self.assign_user:
             assets = self.assets.values('user__username').distinct()
             assets_count = assets.annotate(cnt=Count('user')).count()
             if assets_count not in (0, 1):
