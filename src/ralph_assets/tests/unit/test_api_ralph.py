e3d6a466730c0da76843d221293f010b9c75754d
Kamil Warguła <kwargula@gmail.com>
2014-07-24 15:13:07 +0200

    Add assigned supports to  in api_ralph, refactor test

--- src/ralph_assets/tests/unit/test_api_ralph.py
+++ src/ralph_assets/tests/unit/test_api_ralph.py
@@ -7,82 +7,46 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
-from ralph_assets.models import Asset, AssetCategory
-from ralph_assets.tests.util import (
-    create_asset,
-    create_model,
-    create_warehouse,
-)
-
-
 from ralph_assets.api_ralph import get_asset
+from ralph_assets.tests.utils.assets import (
+    DCAssetFactory,
+    AssetCategoryFactory,
+    AssetModelFactory,
+)
+from ralph_assets.tests.utils.supports import DCSupportFactory
 
 
 class TestApiRalph(TestCase):
     """Test internal API for Ralph"""
 
-    def setUp(self):
-        self.category = AssetCategory.objects.get(name="Blade System")
-        self.model = create_model(
-            category=self.category
-        )
-        self.model2 = create_model(
-            'Model2',
-            category=self.category,
-        )
-        self.warehouse = create_warehouse()
-        self.asset = create_asset(
-            sn='1111-1111-1111-1111'
-        )
-        self.asset_id = self.asset.id
-        self.asset.device_info.ralph_device_id = 666
-        self.asset.device_info.save()
-
-        self.asset_data_raw = {
-            u'asset_id': 1,
-            u'barcode': None,
-            u'category': u'Blade System',
-            u'delivery_date': None,
-            u'deprecation_rate': 25,
-            u'invoice_date': None,
-            u'invoice_no': None,
-            u'is_deprecated': True,
-            u'manufacturer': u'Manufacturer1',
-            u'model': u'Model1',
-            u'niw': None,
-            u'order_no': None,
-            u'price': 0,
-            u'production_use_date': None,
-            u'provider': None,
-            u'provider_order_date': None,
-            u'rack': None,
-            u'remarks': u'',
-            u'request_date': None,
-            u'size': 0,
-            u'slots': 0.0,
-            u'sn': u'1111-1111-1111-1111',
-            u'source': u'shipment',
-            u'status': u'new',
-            u'support_period': 24,
-            u'support_price': None,
-            u'support_type': u'standard',
-            u'support_void_reporting': True,
-            u'u_height': None,
-            u'u_level': None,
-            u'warehouse': u'Warehouse',
-        }
-
     def test_get_asset(self):
         """Test get asset information by ralph_device_id."""
-        asset = Asset.objects.get(pk=self.asset_id)
-        asset_data = get_asset(asset.device_info.ralph_device_id)
-        self.assertEqual(asset_data, self.asset_data_raw)
+        self.support1 = DCSupportFactory()
+        self.support2 = DCSupportFactory()
+        self.category = AssetCategoryFactory()
+        self.model = AssetModelFactory(category=self.category)
+        self.asset = DCAssetFactory(
+            model=self.model,
+            supports=[self.support1, self.support2],
+        )
+        asset_data = get_asset(self.asset.device_info.ralph_device_id)
+        self.assertEqual(asset_data['sn'], self.asset.sn)
+        self.assertEqual(asset_data['barcode'], self.asset.barcode)
+        self.assertEqual(asset_data['supports'][0]['name'], self.support1.name)
+        self.assertEqual(asset_data['supports'][0]['url'], self.support1.url)
+        self.assertEqual(asset_data['supports'][1]['name'], self.support2.name)
+        self.assertEqual(asset_data['supports'][1]['url'], self.support2.url)
+        self.assertEqual(
+            asset_data['required_support'], self.asset.required_support,
+        )
+
+    def test_get_asset_without_asset(self):
+        self.assertEqual(get_asset(666), None)
 
     def test_get_asset_with_empty_asset_source(self):
         """Getting an asset with empty 'source' field should also succeed."""
-        asset = Asset.objects.get(pk=self.asset_id)
-        asset.source = None
-        asset.save()
-        self.asset_data_raw['source'] = None
-        asset_data = get_asset(asset.device_info.ralph_device_id)
-        self.assertEqual(asset_data, self.asset_data_raw)
+        self.category = AssetCategoryFactory()
+        self.model = AssetModelFactory(category=self.category)
+        self.asset = DCAssetFactory(model=self.model, source=None)
+        self.asset_data = get_asset(self.asset.device_info.ralph_device_id)
+        self.assertEqual(self.asset_data['source'], None)
