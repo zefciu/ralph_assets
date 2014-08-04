f93fe00c420af270767a35268eaacc045fcfe899
Slawomir Sawicki <ext_slawomir.sawicki@c50135.allegrogroup.internal>
2014-08-04 11:30:03 +0200

    Changed scrooge API

--- src/ralph_assets/api_pricing.py
+++ src/ralph_assets/api_pricing.py
@@ -5,9 +5,14 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import logging
+
 from django.db.models import Q
 
 from ralph_assets.models_assets import Asset, Warehouse
+
+
+logger = logging.getLogger(__name__)
 
 
 def get_warehouses():
@@ -25,29 +30,30 @@ def get_assets(date):
         Q(invoice_date=None) | Q(invoice_date__lte=date),
         part_info=None,
     ):
+        if not asset.service:
+            logger.error('Asset {0} have no service'.format(asset.id))
+            continue
+        if not asset.warehouse:
+            logger.error('Asset {0} have no warehouse'.format(asset.id))
+            continue
+        if not asset.model:
+            logger.error('Asset {0} have no model'.format(asset.id))
+            continue
+
         device_info = asset.device_info
-
-        venture_info = asset.venture
-        is_blade = None
-        if asset.model and asset.model.category:
-            is_blade = asset.model.category.is_blade
-
         yield {
-            'asset_id': asset.id,
-            'barcode': asset.barcode,
-            'is_deprecated': asset.is_deprecated(date=date),
-            'price': asset.price,
-            'ralph_id': device_info.ralph_device_id if device_info else None,
-            'slots': asset.slots,
-            'sn': asset.sn,
-            'price': asset.price,
-            'deprecation_rate': asset.deprecation_rate,
+            'service_ci_uid': asset.service.ci_uid,
+            'warehouse_id': asset.warehouse.id,
+            'core': asset.cores_count,
             'power_consumption': asset.model.power_consumption,
-            'height_of_device': asset.model.height_of_device,
-            'warehouse_id': asset.warehouse_id,
-            'venture_id': venture_info.id if venture_info else None,
-            'is_blade': is_blade,
-            'cores_count': asset.cores_count,
+            'collocation': asset.model.height_of_device,
+            'sn': asset.sn,
+            'barcode': asset.barcode,
+            'device_id': device_info.ralph_device_id if device_info else None,
+            'depreciation_rate': asset.deprecation_rate,
+            'is_depreciated': asset.is_deprecated(date=date),
+            'price': asset.price,
+            'asset_name': asset.hostname
         }
 
 
