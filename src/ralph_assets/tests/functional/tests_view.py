f12ed9f895b6bc554e2fb931fb821937f273e1e8
Tymoteusz Jankowski <tymoteusz.jankowski@ext.allegro.pl>
2014-08-05 14:55:54 +0200

    rm duplicated ACLInheritanceTest

--- src/ralph_assets/tests/functional/tests_view.py
+++ src/ralph_assets/tests/functional/tests_view.py
@@ -1011,35 +1011,6 @@ class DeviceEditViewTest(TestCase):
         self.assertContains(response, part)
 
 
-class ACLInheritanceTest(TestCase):
-
-    def test_all_views_inherits_acls(self):
-        """
-        - get all views from url.py except these urls:
-            - api (until it clarifies)
-            - redirections
-        - assert if each view has ACLClass in mro
-        """
-        from ralph_assets import urls
-        from ralph_assets.views.base import ACLGateway
-        excluded_urls_by_regexp = [
-            '^api/'  # skip it until api authen./author. is resolved
-        ]
-        for urlpattern in urls.urlpatterns:
-            if urlpattern._regex in excluded_urls_by_regexp:
-                continue
-            elif urlpattern.callback.func_name == 'RedirectView':
-                continue
-            module_name = urlpattern._callback.__module__
-            class_name = urlpattern._callback.__name__
-            imported_module = __import__(module_name, fromlist=[class_name])
-            found_class = getattr(imported_module, class_name)
-            msg = "View '{}' doesn't inherit from acl class".format(
-                '.'.join([module_name, class_name])
-            )
-            self.assertIn(ACLGateway, found_class.__mro__, msg)
-
-
 class TestImport(TestCase):
     def setUp(self):
         self.client = login_as_su()
