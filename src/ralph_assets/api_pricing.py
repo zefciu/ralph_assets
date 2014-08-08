e67c383f2f88a7bcbc1dd4a82bcb74ac9bc01254
Mateusz Kurek <master.mateusz@gmail.com>
2014-08-08 11:55:24 +0200

    added environment to scrooge api

--- src/ralph_assets/api_pricing.py
+++ src/ralph_assets/api_pricing.py
@@ -31,17 +31,20 @@ def get_assets(date):
         part_info=None,
     ):
         device_info = asset.device_info
-        if not asset.service:
-            logger.error('Asset {0} have no service'.format(asset.id))
-            continue
         if not asset.warehouse:
-            logger.error('Asset {0} have no warehouse'.format(asset.id))
+            logger.error('Asset {0} has no warehouse'.format(asset.id))
             continue
         if not asset.model:
-            logger.error('Asset {0} have no model'.format(asset.id))
+            logger.error('Asset {0} has no model'.format(asset.id))
             continue
         if not asset.device_info:
-            logger.error('Asset {0} have no device'.format(asset.id))
+            logger.error('Asset {0} has no device'.format(asset.id))
+            continue
+        if not asset.service:
+            logger.error('Asset {0} has no service'.format(asset.id))
+            continue
+        if not asset.device_environment:
+            logger.error('Asset {0} has no environment'.format(asset.id))
             continue
         else:
             hostname = None
@@ -51,19 +54,20 @@ def get_assets(date):
                     hostname = ralph_device.name
 
         yield {
-            'service_ci_uid': asset.service.uid,
-            'warehouse_id': asset.warehouse.id,
-            'core': asset.cores_count,
-            'power_consumption': asset.model.power_consumption,
-            'collocation': asset.model.height_of_device,
-            'sn': asset.sn,
-            'barcode': asset.barcode,
             'asset_id': asset.id,
             'device_id': device_info.ralph_device_id if device_info else None,
+            'asset_name': hostname,
+            'service_ci_uid': asset.service.uid,
+            'environment_id': asset.device_environment.id,
+            'sn': asset.sn,
+            'barcode': asset.barcode,
+            'warehouse_id': asset.warehouse.id,
+            'cores_count': asset.cores_count,
+            'power_consumption': asset.model.power_consumption,
+            'collocation': asset.model.height_of_device,
             'depreciation_rate': asset.deprecation_rate,
             'is_depreciated': asset.is_deprecated(date=date),
             'price': asset.price,
-            'asset_name': hostname,
         }
 
 
