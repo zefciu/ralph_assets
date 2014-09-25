98265edc9db1b0a8afce3d8c867305226080baaf
Arkadiusz Adamski <arkadiusz.adamski@ext.allegrogroup.com>
2014-09-25 10:59:26 +0200

    added sidebar to history view

--- src/ralph_assets/history/views.py
+++ src/ralph_assets/history/views.py
@@ -13,14 +13,10 @@ from ralph_assets.models_assets import ASSET_TYPE2MODE
 
 
 class HistoryBase(AssetsBase):
-    mainmenu_selected = 'unknown'
-    sidebar_selected = None
-
-    def get_section(self):
+    @property
+    def active_submodule(self):
         if str(self.content_type) == 'asset':
-            return ASSET_TYPE2MODE[self.content_type.get_object_for_this_type(
-                id=self.object_id
-            ).type]
+            return 'hardware'
         mapper = {
             'licence': 'licences',
             'support': 'supports',
@@ -28,14 +24,19 @@ class HistoryBase(AssetsBase):
         return mapper[str(self.content_type)]
 
     def get_context_data(self, **kwargs):
+        mode = ASSET_TYPE2MODE[self.content_type.get_object_for_this_type(
+            id=self.object_id
+        ).type]
         context = super(HistoryBase, self).get_context_data(**kwargs)
-        context.update({'section': self.get_section()})
+        sidebars = context['active_menu'].get_sidebar_items()
+        context.update({
+            'sidebar': sidebars['hardware_{}'.format(mode)],
+        })
         return context
 
 
 class HistoryListForModel(PaginateMixin, ContentTypeMixin, HistoryBase):
     """View for history of object."""
-    submodule_name = 'unknown'
     template_name = 'assets/history/history_for_model.html'
 
     def dispatch(self, request, *args, **kwargs):
