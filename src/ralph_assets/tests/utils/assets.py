4e036f7e8f2cde3309afe313b335f573db723937
Tymoteusz Jankowski <tymoteusz.jankowski@ext.allegro.pl>
2014-10-13 10:37:36 +0200

    changed asset factories to include default region

--- src/ralph_assets/tests/utils/assets.py
+++ src/ralph_assets/tests/utils/assets.py
@@ -19,12 +19,10 @@ from factory import (
     SubFactory,
 )
 from factory.django import DjangoModelFactory
+from ralph.account.models import Region
 from ralph.cmdb.tests.utils import (
     DeviceEnvironmentFactory,
     ServiceCatalogFactory,
-)
-from ralph.util.tests.utils import (
-    RegionFactory,
 )
 
 from ralph_assets import models_assets
@@ -187,7 +185,11 @@ class AssetFactory(DjangoModelFactory):
     provider = Sequence(lambda n: 'Provider #%s' % n)
     support_period = 24
     support_type = 'standard'
-    region = SubFactory(RegionFactory)
+
+    @lazy_attribute
+    def region(self):
+        # lazy attr because static fails (it's not accessible during import)
+        return Region.get_default_region()
 
     @lazy_attribute
     def sn(self):
@@ -217,7 +219,6 @@ class BaseAssetFactory(DjangoModelFactory):
     property_of = SubFactory(AssetOwnerFactory)
     provider = Sequence(lambda n: 'Provider #%s' % n)
     provider_order_date = fuzzy.FuzzyDate(datetime.date(2008, 1, 1))
-    region = SubFactory(RegionFactory)
     remarks = Sequence(lambda n: 'Remarks #{}'.format(n))
     request_date = fuzzy.FuzzyDate(datetime.date(2008, 1, 1))
     service = SubFactory(ServiceCatalogFactory)
@@ -247,6 +248,11 @@ class BaseAssetFactory(DjangoModelFactory):
             # A list of supports were passed in, use them
             for support in extracted:
                 self.supports.add(support)
+
+    @lazy_attribute
+    def region(self):
+        # lazy attr because static fails (it's not accessible during import)
+        return Region.get_default_region()
 
 
 class DCAssetFactory(BaseAssetFactory):
