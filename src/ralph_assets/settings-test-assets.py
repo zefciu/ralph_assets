6b3da6a03744f096ce19f5be6146b251baaaaff1
Arkadiusz Adamski <arkadiusz.adamski@ext.allegrogroup.com>
2015-01-12 13:00:19 +0100

    added build matrix

---
+++ src/ralph_assets/settings-test-assets.py
@@ -0,0 +1,31 @@
+"""A testing profile."""
+
+import os
+
+if os.environ.get('TEST_DATABASE_ENGINE') == 'mysql':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'ralph',
+            'USER': 'root',
+            'HOST': 'localhost',
+        },
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+            'USER': '',
+            'PASSWORD': '',
+            'HOST': '',
+            'PORT': '',
+            'OPTIONS': {},
+        }
+    }
+
+PLUGGABLE_APPS = ['cmdb', 'assets']
+
+SOUTH_TESTS_MIGRATE = False
+
+ASSETS_AUTO_ASSIGN_HOSTNAME = True
