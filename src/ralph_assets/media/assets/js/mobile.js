20689bce6ccd516f453319b6f8569004b971bc01
Marcin Kliks <marcin.kliks@allegro.pl>
2015-03-03 11:41:18 +0100

    Mobile responsive version for assed editing.
    The changes provide some minimal support for mobile app which embeds
    edit view into the mobile application.
    Edit view opens without menu bars, without search bar, to be embedded
    easily.

---
+++ src/ralph_assets/media/assets/js/mobile.js
@@ -0,0 +1,11 @@
+(function() {
+	"use strict";
+	if ($(window).width() < 768) {
+		$(window).load(function() {
+			// Re-allign data center information in the mobile app
+			// to the top. To be changed later to correctly reallign
+			// using native views api.
+			$("div.additional-info").insertBefore("div.basic-info");
+		})
+	}
+})();
