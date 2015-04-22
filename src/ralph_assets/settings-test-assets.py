afadd351d178eb35847a70ed443e62e801d5be49
Tymoteusz Jankowski <tymoteusz.jankowski@ext.allegro.pl>
2015-04-22 11:29:04 +0200

    Changed STATICFILES_STORAGE to non-chached.
    Recently STATICFILES_STORAGE was changed to CachedStaticFilesStorage in
    Ralph-core. So two changes:
    - update on assets test settings (to fix tests),
    - changed url generation method (to get full benefit from the change).

--- src/ralph_assets/settings-test-assets.py
+++ src/ralph_assets/settings-test-assets.py
@@ -13,3 +13,5 @@ PLUGGABLE_APPS = ['cmdb', 'assets']
 SOUTH_TESTS_MIGRATE = False
 
 ASSETS_AUTO_ASSIGN_HOSTNAME = True
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
